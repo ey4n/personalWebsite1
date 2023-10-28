4c82b2d25105128cf62338ae6b5a9d3f2e3e6728
eyan c <eyanc@eyans-MacBook-Pro.local>
2023-10-28 22:34:10 +0800

    background mobile 32

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const NavigationBar: React.FC = () => {
   return (
-    <nav style={{ position: 'fixed', top: 0, left: 0, right: 0, backgroundColor: '#f2f2f2', padding: '30px',zIndex:'2'}}>
+    <nav style={{ position: 'fixed', top: 0, left: 0, right: 0, backgroundColor: '#f2f2f2', padding: '10px',zIndex:'2'}}>
       <ul style={{ display: 'flex', justifyContent: 'flex-end', listStyle: 'none', margin: 0, padding: 0}}>
         <li style={{ margin: '0 20px' }}>
           <Link to="/" style ={{fontFamily: 'serif', color: '#1550B0'}}>Home</Link>
