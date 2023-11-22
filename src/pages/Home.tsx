b678624bd1682f49fb18bc6d1a941b1e53e2b83b
ey4n <ey4n0407@gmail.com>
2023-11-22 21:38:04 +0800

    resizing home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from "react";
-import {useMediaQuery} from '@react-hooks-library/core';
+import { useMediaQuery } from '@react-hooks-library/core';
 import "../App.css";
 import myFace from '../images/myFace.jpeg';
 import { Link } from 'react-router-dom';
-import backgroundVideo from '../images/background.mp4'
+import backgroundVideo from '../images/background.mp4';
+
 const Home: React.FC = () => {
   const isPageSmall = useMediaQuery('(max-width: 600px)');
-  
+
   return (
-    <div className="home">
+    <div className={`home ${isPageSmall ? 'mobile-view' : ''}`}>
       {isPageSmall ? (
         <div className="mobile-background"></div>
       ) : (
@@ -16,22 +17,25 @@ const Home: React.FC = () => {
           <source src={backgroundVideo} type="video/mp4" />
         </video>
       )}
-      <div style={{position: 'relative',zIndex: '1'}}>
-      <h1 style={{fontFamily:'serif',color:'#525453'}}>Hi! Welcome to my website</h1>
-      <div style={{display:'flex', alignItems:'center', width: '600px', justifyContent: 'space-between', gap: '30px'}}>
-        <img src={myFace} alt="My Face" width={200} height={250} style={{borderRadius:'100px'}}/>
-        <div style = {{display:'flex', flexDirection: 'column', alignItems: 'center'}}>
-        <p style={{backgroundColor: '#def4fa',padding:'30px',borderRadius:'10px',fontFamily:'serif',color:'#525453'}}>
-          Hi, I'm Eyan, a Computer Engineering student under Nanyang Technological University's flagship Renaissance Engineering Programme.
-        </p>
-        <Link className='button'
-        to="https://www.linkedin.com/in/eyan-chua"
-        style={{ backgroundColor: '#def4fa', fontFamily: 'serif', display: 'block', padding: '10px', borderRadius: '10px' }}
-      >
-        Click here to connect!
-      </Link>
+      <div style={{ position: 'relative', zIndex: '1' }}>
+        <h1 style={{ fontFamily: 'serif', color: '#525453', fontSize: isPageSmall ? '1.5em' : '2em' }}>
+          Hi! Welcome to my website
+        </h1>
+        <div style={{ display: 'flex', flexDirection: isPageSmall ? 'column' : 'row', alignItems: 'center', justifyContent: isPageSmall ? 'center' : 'space-between', gap: '30px' }}>
+          <img src={myFace} alt="My Face" width={isPageSmall ? 150 : 200} height={isPageSmall ? 180 : 250} style={{ borderRadius: '100px' }} />
+          <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <p style={{ backgroundColor: '#def4fa', padding: isPageSmall ? '15px' : '30px', borderRadius: '10px', fontFamily: 'serif', color: '#525453' }}>
+              Hi, I'm Eyan, a Computer Engineering student under Nanyang Technological University's flagship Renaissance Engineering Programme.
+            </p>
+            <Link
+              className='button'
+              to="https://www.linkedin.com/in/eyan-chua"
+              style={{ backgroundColor: '#def4fa', fontFamily: 'serif', display: 'block', padding: '10px', borderRadius: '10px' }}
+            >
+              Click here to connect!
+            </Link>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
